ca4156bbc37eae9769714cfdde5a27e8188cc185
johanliebert79 <anhhungduy33@gmail.com>
2024-12-18 00:36:22 +0700

    owo

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.lang.foreign.SymbolLookup;
+import java.util.Locale;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
@@ -22,5 +25,33 @@ public class Main {
         climbingStairsSolution solution1 = new climbingStairsSolution();
         int inputStairs = solution1.climbStairs(random.nextInt(10));
         System.out.println("Total stairs: " + inputStairs);
+
+        System.out.println("Hi here is the funny game: ");
+        System.out.println("The rule is simple, you will guess a number from 1 to 100, Number Guessing Game :) ");
+        Scanner scanner = new Scanner(System.in);
+        Scanner inputFromUser = new Scanner(System.in);
+
+        System.out.println("So choose difficulty :)");
+        while (true) {
+            System.out.println("\nCommands: easy, medium, hard");
+            System.out.print("> ");
+            String command = scanner.next().toLowerCase(Locale.ROOT);
+
+            switch (command) {
+                case "easy" -> {
+                    System.out.println("You have 10 chances :) good luck! ");
+                    System.out.println("What numbers are you thinking?");
+                    int number = inputFromUser.nextInt();
+                    if (number == random.nextInt(0,1)) {
+                        System.out.println("Wao, you are so good");
+                    }
+                    else {
+                        System.out.println("So bad");
+                }
+            }
+                default -> throw new IllegalStateException("Unexpected value: " + command);
+            }
+
     }
+}
 }
