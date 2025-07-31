f5c28d938160ad3f79605f272e801e0df60ba362
admin <ntduy@yahoo.com>
2025-07-31 23:03:04 +0700

    Greedy_680

---
+++ src/validPalindromeII.java
@@ -0,0 +1,44 @@
+/* Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+Example 3:
+
+Input: s = "abc"
+Output: false */
+
+public class validPalindromeII {
+
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Thử bỏ 1 ký tự ở trái hoặc phải
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
